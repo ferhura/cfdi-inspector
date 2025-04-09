83a0afd09da633cbde1ca6d875555c3ba57e7560
Fernando Hurtado <ferhura@gmail.com>
2025-04-08 23:47:11 -0600

    Se ajusto SignUp

--- apps/frontend/app/register/page.tsx
+++ apps/frontend/app/register/page.tsx
@@ -4,7 +4,7 @@ import { SignUp } from '@clerk/nextjs';
 export default function RegisterPage() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <SignUp path="/register" routing="path" signInUrl="/login" />
+      <SignUp routing="hash" signInUrl="/login" />
     </div>
   );
 }
